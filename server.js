f42c3d338a11bc200a3c57379cc0cf58bfbb3c7c
Smeet Patel <smeetpatel@Smeets-MacBook-Pro.local>
2023-04-27 17:12:50 +0530

    chat-app

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ import { makeExecutableSchema } from "@graphql-tools/schema";
 import path from 'path'
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-// console.log(process.env.PORT);
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
